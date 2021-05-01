bc229798419ca78a7958aacdcf462f38029f55e6
Jessica <simply.me05@hotmail.com>
2021-05-01 07:48:04 -0400

    worked on seeds files

--- Develop/seeds/userSeeds.js
+++ Develop/seeds/userSeeds.js
@@ -1,17 +1,32 @@
-[
+const { User } = require('../models');
+
+const userData = [
   {
-    "name": "Sal",
-    "email": "sal@hotmail.com",
-    "password": "password12345"
+    name: 'Betty',
+    email: 'betty@hotmail.com',
+    password: 'password12345',
   },
   {
-    "name": "Lernantino",
-    "email": "lernantino@gmail.com",
-    "password": "password12345"
+    name: 'Tommy',
+    email: 'tommy@hotmail.com',
+    password: 'password12345',
   },
   {
-    "name": "Amiko",
-    "email": "amiko2k20@aol.com",
-    "password": "password12345"
-  }
-]
+    name: 'Sue',
+    email: 'sue@hotmail.com',
+    password: 'password12345',
+  },
+  {
+    name: 'Lernard',
+    email: 'lernard@gmail.com',
+    password: 'password12345',
+  },
+  {
+    name: 'Miko',
+    email: 'miko2k20@aol.com',
+    password: 'password12345',
+  },
+];
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
